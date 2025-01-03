0d1a5cb95344c21503ab1a90c7e7603c6c82ffa2
butterr12 <126962777+butterr12@users.noreply.github.com>
2025-01-03 09:08:26 +0800

    Add Player Counter

--- python_client/src/controller/input_handler.py
+++ python_client/src/controller/input_handler.py
@@ -1,6 +1,7 @@
 import pygame
 from model.board import Board
 from model.piece import Piece
+from model.game import Game
 
 class InputHandler:
     def __init__(self):
@@ -15,8 +16,17 @@ class InputHandler:
             grid_x, grid_y = y // 100, x // 100
             print(f"row: {grid_x}")
             print(f"col: {grid_y}")
+            clicked_piece = game.board.grid[grid_x][grid_y]
 
-            if self.selected_piece:
+            if clicked_piece and clicked_piece.owner == game.current_player:
+                # reselect new piece if misclicked/change of mind
+                self.selected_piece = clicked_piece
+                self.valid_moves = self.selected_piece.valid_moves((grid_x, grid_y), game.board)
+                self.orig_coords = (grid_x, grid_y)
+                print(f"Switched to new piece: {self.selected_piece}")
+                print(f"Valid moves: {self.valid_moves}")
+                    
+            elif self.selected_piece:
                 print(self.selected_piece)
                 # Check if the clicked position is a valid move
                 if (grid_x, grid_y) in self.valid_moves:
@@ -24,8 +34,10 @@ class InputHandler:
                     print(grid_y)
                     print("entered movepiece")
                     game.board.move_piece(self.orig_coords, (grid_x, grid_y))
+                    game.increment_counter()
                     self.selected_piece = None
                     self.valid_moves = []  # Reset valid moves after the move
+                    self.orig_coords = None
                 else:
                     print("Invalid move!")
             else:
--- python_client/src/model/game.py
+++ python_client/src/model/game.py
@@ -4,10 +4,19 @@ class Game:
     def __init__(self):
         self.board = Board()
         self.current_player = "Player 1"
+        self.counter = 0
+        self.max_moves = 3
         self.winner = None
 
     def switch_player(self):
         self.current_player = "Player 2" if self.current_player == "Player 1" else "Player 1"
+    
+    def increment_counter(self):
+        self.counter += 1
+        if self.counter >= self.max_moves:
+            self.switch_player()
+            self.counter = 0
+        print(self.counter)
 
     def check_winner(self):
         # Check win conditions (e.g., Lion capture)
